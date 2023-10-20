e6a23f092b27b52b18cb7c489c5fa7955becdf90
Nathan A <fRenzyA05@gmail.com>
2023-10-20 15:30:36 -0300

    updated player script preventing builds

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    MapGenerator mapGenerator;
     Rigidbody rb;
     Vector3 velocity;
 
     void Start()
     {
-        mapGenerator = GameObject.Find("MapGenerator").GetComponent<MapGenerator>();
+        
         rb = GetComponent<Rigidbody>();
     }
 
